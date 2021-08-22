3095deb8f9e82e3f1de9da5062268c7795487ba2
moto4321 <moto432195@gmail.com>
2021-08-22 17:27:41 +0900

    register page

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 const express = require('express')
 const mysql = require('mysql')
 const dotenv = require('dotenv')
+const path = require('path')
 
 dotenv.config({ path: './.env' })
 
 const app = express()
 
+// const db = mysql.createConnection({
+//   host: process.env.HOST,
+//   user: process.env.USER,
+//   password: process.env.PASSWORD,
+//   database: process.env.DATABASE
+// })
+
 const db = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE
+  host: 'localhost',
+  user: 'root',
+  password: 'dntjr13570',
+  database: 'test'
 })
+
+const publicDirectory = path.join(__dirname, './public')
+app.use(express.static(publicDirectory))
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: false })) // req.body가 undefined로 나오는것을 막음
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json())
 
 app.set('view engine', 'hbs')
 
@@ -23,9 +39,9 @@ db.connect((err) => {
   }
 })
 
-app.get('/', (req, res) => {
-  res.send("<h1>Home Page</h1>")
-})
+//Define Routes
+app.use('/', require('./routes/pages'))
+app.use('/auth', require('./routes/auth'))
 
 app.listen(5000, function(){
   console.log('server started on port 5000')
---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const authController = require('../controllers/auth')
+
+const router = express.Router()
+
+// /auth/register
+router.post('/register', authController.register )
+
+
+module.exports = router
